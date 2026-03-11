d15fb8f45c4f910cf4f9491cda7ce9150e24d211
sumailjunior03-ui <sumailjunior03@gmail.com>
2026-03-11 21:55:14 +0500

    updated

--- network.js
+++ network.js
@@ -3,18 +3,8 @@
    All sites update automatically across the utility network. */
 
 window.CALCHQ_NETWORK = [
-  { name: "Calc-HQ",                url: "https://calc-hq.com" },
-  { name: "BizDayChecker.com",      url: "https://bizdaychecker.com" },
-  { name: "BankCutoffChecker.com",  url: "https://bankcutoffchecker.com" },
-  { name: "SalaryVsInflation.com",  url: "https://salaryvsinflation.com" },
-  { name: "hourly2salarycalc.com",  url: "https://hourly2salarycalc.com" },
-  { name: "PayrollDateChecker.com", url: "https://payrolldatechecker.com" },
-  { name: "1099vsw2calc.com",       url: "https://1099vsw2calc.com" },
-  { name: "FreelanceIncomeCalc.com", url: "https://freelanceincomecalc.com" },
-  { name: "QuarterlyTaxCalc.com",   url: "https://quarterlytaxcalc.com" },
-  { name: "TotalCompCalc.com",      url: "https://totalcompcalc.com" },
-  { name: "OvertimePayCalc.com",    url: "https://overtimepaycalc.com" },
-  { name: "AfterTaxSalaryCalc.com", url: "https://aftertaxsalarycalc.com" },
+  { name: "Calc-HQ",          url: "https://calc-hq.com" },
+  { name: "1099vsW2Calc.com", url: "https://1099vsw2calc.com" },
 ];
 
 (function () {
